0eec920584c4e6f55f319639a56230b29fc3aaec
Srilal Sachintha <SrilalSachintha@Gmail.com>
2023-01-13 18:31:46 +0530

    formats

--- resources/js/Views/Home.vue
+++ resources/js/Views/Home.vue
@@ -6,21 +6,22 @@
                     <v-icon size="128" color="white">mdi-rocket-launch</v-icon>
                 </v-card>
 
-                <v-alert class="mt-4">
+                <v-alert class="my-4">
                     <v-alert-title>
                         <span class="text-red text-h2 font-weight-medium">Laravel Jet Fuel</span>
                     </v-alert-title>
                 </v-alert>
 
                 <v-row no-gutters>
-                    <v-card>
-                        <v-img src="/Assets/laravel.svg')"></v-img>
+                    <v-card width="200" v-for="(tech,i) in techs" :key="i" class="pa-2 mr-2">
+                        <v-img height="64" :src="tech.img"></v-img>
+                        <v-card-title>
+                            <v-row no-gutters justify="center">
+                                {{ tech.version }}
+                            </v-row>
+                        </v-card-title>
                     </v-card>
                 </v-row>
-                <br>
-
-                <br>
-                <span>Laravel 9 | Vue 3 | Vuetify 3</span>
                 <br>
                 <span>Template Maintainer: Srilal Sachintha</span>
                 <br>
@@ -37,8 +38,15 @@
 
 export default {
     name: "Home",
-    methods:{
-    }
+    data: () => ({
+        techs: [
+            { name: "Laravel", img: "/assets/laravel.svg", version:'Laravel 9' },
+            { name: "Vue", img: "/assets/vue.svg", version: 'Vue 3' },
+            { name: "Vuetify", img: "/assets/vuetify.svg", version: 'Vuetify 3' },
+            { name: "Pinia", img: "/assets/pinia.svg", version: 'Pinia 3' },
+
+        ],
+    }),
 }
 </script>
 
